aad6e28241fb830b373edb23bbb508483c84b59f
Alif <alifwr98@gmail.com>
2025-12-19 20:20:25 +0700

    update task2

--- task2_llm_rag/main.py
+++ task2_llm_rag/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from langchain_core.messages import HumanMessage, AIMessage
 
-from schema import ChatRequest, ChatResponse
+from schemas.api import ChatRequest, ChatResponse
 
 from workflow import get_workflow
 
@@ -9,8 +10,35 @@ app = FastAPI()
 
 @app.post("/chat", response_model=ChatResponse)
 async def chat(request: ChatRequest):
-    messages = request.messages
+    messages = [
+        HumanMessage(content=m.content)
+        if m.role == "user"
+        else AIMessage(content=m.content)
+        for m in request.messages
+    ]
+
     workflow = get_workflow()
 
-    result = workflow.invoke(messages)
-    return ChatResponse(messages=result)
+    result = workflow.invoke(
+        {"messages": messages, "is_input_safe": True, "is_output_safe": True}
+    )
+
+    response_messages = []
+    for m in result["messages"]:
+        msg_dict = {
+            "role": "assistant" if m.type == "ai" else "user",
+            "content": str(m.content),
+        }
+        if m.type == "tool":
+            msg_dict["role"] = "tool"
+            msg_dict["tool_call_id"] = m.tool_call_id
+            msg_dict["name"] = m.name
+        elif m.type == "ai":
+            if getattr(m, "tool_calls", None):
+                msg_dict["tool_calls"] = m.tool_calls
+
+        print(msg_dict)
+
+        response_messages.append(msg_dict)
+
+    return ChatResponse(messages=response_messages)
--- task2_llm_rag/schemas/api.py
+++ task2_llm_rag/schemas/api.py
@@ -1,9 +1,21 @@
+from typing import Optional, Any, Literal
 from pydantic import BaseModel
 
 
+class Message(BaseModel):
+    role: Literal["user", "assistant", "tool"]
+    content: str
+
+
+class MessageWithTool(Message):
+    tool_calls: Optional[list[dict[str, Any]]] = None
+    tool_call_id: Optional[str] = None
+    name: Optional[str] = None
+
+
 class ChatRequest(BaseModel):
-    messages: list[dict[str, str]]
+    messages: list[Message]
 
 
 class ChatResponse(BaseModel):
-    messages: list[dict[str, str]]
+    messages: list[MessageWithTool]
